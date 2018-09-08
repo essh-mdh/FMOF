c82c297a751ef42e8703486f412dec590251d9d4
Hossein Fotouhi <hossein.fotouhi@mdh.se>
2018-09-08 10:27:16 +0200

    Initial commit

---
+++ examples/smart-HOP/rpl-udp-client/project-conf.h
@@ -0,0 +1,6 @@
+
+/*smart-HOP Configurations*/
+#define MOBILE_NODE 1
+#define RPL_CONF_LEAF_ONLY 1
+#define MN_CONNECTION 0
+
---
+++ examples/smart-HOP/rpl-udp-server/project-conf.h
@@ -0,0 +1,12 @@
+/*#define NETSTACK_CONF_RDC_CHANNEL_CHECK_RATE 64*/
+#define NETSTACK_CONF_RDC nullrdc_driver
+#define NULLRDC_802154_AUTOACK 0
+#define NULLRDC_802154_AUTOACK_HW 0
+#define RPL_MAX_PARENTS_PER_DAG 10
+#define RPL_CONF_DAO_ACK 1
+#define PROCESS_CONF_NO_PROCESS_NAMES 1
+#define UIP_CONF_TCP 1
+
+/* smart-HOP configurations*/
+#define MOBILE_NODE 0
+#define RPL_CONF_LEAF_ONLY 0
